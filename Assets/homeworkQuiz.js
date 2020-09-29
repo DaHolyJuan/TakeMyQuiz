67989c297c3f3aef74288502ef13513f8f87831a
DaHolyJuan <juan.3scalant3@gmail.com>
2020-09-28 19:30:24 -0500

    Added files to first commit

---
+++ Assets/homeworkQuiz.js
@@ -0,0 +1,6 @@
+var timeEl = document.querySelector(".timer");
+var mainEl = document.querySelector(".main");
+var contentEl = document.querySelector(".content");
+var startButtonEl = document.querySelector(".start");
+var verifyEl = document.querySelector(".verify");
+var initialsEl =document.querySelector(".initials");
