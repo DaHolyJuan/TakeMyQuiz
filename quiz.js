5f49c174665a0c5356262d12073d64705d6403cf
DaHolyJuan <juan.3scalant3@gmail.com>
2020-09-29 19:20:24 -0500

    added variables and questions to start quiz

---
+++ quiz.js
@@ -0,0 +1,40 @@
+//this variable is for where the user is at 
+var position = 0;
+//this variable holds how many correct 
+var correct = 0;
+//these variables hold the div, header, question, user answer, and "a, b, c, d"
+var quiz, quizStatus, question, choice, choices, answer1, answer2, answer3, answer4;
+// this var is our "array" of questions, with choices and correct answer
+var questions = [
+    {
+        question: "Question 1 will go here!",
+        a: "answer1",
+        b: "answer2",
+        c: "answer3",
+        d: "answer4",
+        answer: "A"
+
+    },
+    {
+        question: "Question 2 will go here!",
+        a: "answer1",
+        b: "answer2",
+        c: "answer3",   
+        d: "answer4",
+    },
+    {
+        question: "question 3 will go here!",
+        a: "answer1",
+        b: "answer2",
+        c: "answer3",
+        d: "answer4",
+    },
+    {
+        question: "question 4 will go here!",
+        a: "answer1",
+        b: "answer2",
+        c: "answer3",
+        d: "answer4",
+    }
+];
+
