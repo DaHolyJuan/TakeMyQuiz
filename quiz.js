e4d28042787355e21ad85835baf08f5493ffc5e1
DaHolyJuan <juan.3scalant3@gmail.com>
2020-09-29 19:42:30 -0500

    added a few more functions to display my questions and position

--- quiz.js
+++ quiz.js
@@ -21,6 +21,7 @@ var questions = [
         b: "answer2",
         c: "answer3",   
         d: "answer4",
+        answer: "B"
     },
     {
         question: "question 3 will go here!",
@@ -28,6 +29,7 @@ var questions = [
         b: "answer2",
         c: "answer3",
         d: "answer4",
+        answer: "C"
     },
     {
         question: "question 4 will go here!",
@@ -35,6 +37,38 @@ var questions = [
         b: "answer2",
         c: "answer3",
         d: "answer4",
+        answer: "D"
     }
 ];
+
+function get(x) {
+    return document.getElementById(x);
+}
+
+function renderQuestion() {
+    quiz = get("quiz");
+    if (position >= questions.length) {
+        quiz.innerHTML = "<h2>You got " + correct + " of " + questions.length +" questions correct!</h2>";
+        get("quizStatus").innerHTML = "Quiz Finished! You did it!"
+        position = 0;
+        correct = 0;
+        return false;
+    }
+    //this function lets me know position of question user is on
+    get("quizStatus").innerHTML = "Question " + (position+1) + " of" + questions.length;
+    
+    question = questions[position].question;
+    answer1 = questions[position].a;
+    answer2 = questions[position].b;
+    answer3 = questions[position].c;
+    answer4 = questions[position].d;
+
+    //this function displays the answer onto the div "quiz"
+    quiz.innerHTML = "<h2>"+question+"</h2>"
+
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+ answer1 +"</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+ answer2 +"</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+ answer3 +"</label><br><br>";
+    quiz.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
 
